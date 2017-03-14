17cca7eaef3d75352132092620128979ff51400e
TheManWithTheGit <wizzcomm@gmail.com>
2017-03-14 15:50:23 +0000

    Added noise filter
    Added the noise filter as a function, so you can call it in your function for each task. Should work

--- Master.ino
+++ Master.ino
@@ -48,3 +48,48 @@ void LINE_func(void) {
 void TILT_func(void) {
 	//todo, merged from other branches.
 }
+
+//outputs an int, takes in an int, takes median of input values
+int noiseFilter(int number) //noise filter so that the robot doesn't go crazy because of noise
+{
+	boolean swap = true; //setting up the switch statement for later
+	int temp, i, j, counter = 0; //now making my variables
+	int bufferOne[5] = {}; //this is where all the values go in
+	int bufferTwo[5] = {}; //this is for when I need to sort the buffer
+
+	for (i = 0; i <= 3; i++) //prevents the buffer from overflowing
+	{
+		bufferOne[i] = bufferOne[i + 1]; //shifts all the numbers down a cell
+	}
+
+	bufferOne[4] = number; //save the new number to the end of the buffer
+
+	if (counter < 5) //trigger when the buffer is full
+	{
+		counter++;
+			return number;
+	}
+	else //when the buffer is full, sort
+	{
+		for (i = 0; i < 5; i++)
+		{
+			bufferTwo[i] = bufferOne[i]; //puttng values into other buffer
+		}
+		while (swap) //start of the sorting part
+		{
+			swap = false; //sets the loop to run once
+			j++;
+			for (i = 0; i < 4 - j; i++) //takes lowest value and moves it
+			{
+				if (bufferTwo[i] > bufferTwo[i + 1])
+				{
+					temp = bufferTwo[i];
+					bufferTwo[i] = bufferTwo[i + 1];
+					bufferTwo[i + 1] = temp;
+					swap = true;
+				}
+			}
+		}
+		return bufferTwo[2]; //once the sorting is done, returns the median value
+	}
+}
