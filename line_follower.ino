3a9b7779a1060ec80b388ddce298a2b4d16829a8
b_bomb <abarcovschi@gmail.com>
2017-03-15 13:27:31 +0000

    line_follower_using_calculus

---
+++ line_follower.ino
@@ -0,0 +1,96 @@
+#include <Pololu3pi.h>
+#include <PololuQTRSensors.h>
+#include <OrangutanMotors.h>
+#include <OrangutanAnalog.h>
+#include <OrangutanLEDs.h>
+#include <OrangutanLCD.h>
+#include <OrangutanPushbuttons.h>
+#include <OrangutanBuzzer.h>
+void motors_init(void);
+
+OrangutanLCD lcd;
+OrangutanAnalog analog;
+
+Pololu3pi robot;
+unsigned int sensors[5]; // an array to hold sensor values
+unsigned int last_proportional = 0;
+long integral = 0;
+int counter = 0;
+
+void LINE_func(void) {
+ //defining and initialising ldrs for line follow to zero.
+  while (1) {
+  // Get the position of the line.  Note that we *must* provide
+  // the "sensors" argument to read_line() here, even though we
+  // are not interested in the individual sensor readings.
+  unsigned int position = robot.readLine(sensors, IR_EMITTERS_ON);
+
+  // The "proportional" term should be 0 when we are on the line.
+  int proportional = (int)position - 2000;
+
+  // Compute the derivative (change) and integral (sum) of the
+  // position.
+  int derivative = proportional - last_proportional;
+  integral += proportional;
+
+  // Remember the last position.
+  last_proportional = proportional;
+
+  // Compute the difference between the two motor power settings,
+  // m1 - m2.  If this is a positive number the robot will turn
+  // to the right.  If it is a negative number, the robot will
+  // turn to the left, and the magnitude of the number determines
+  // the sharpness of the turn.  You can adjust the constants by which
+  // the proportional, integral, and derivative terms are multiplied to
+  // improve performance.
+  int power_difference = proportional/20 + integral/10000 + derivative*3/2;
+
+  // Compute the actual motor settings.  We never set either motor
+  // to a negative value.
+  const int maximum = 60;
+  if (power_difference > maximum)
+    power_difference = maximum;
+  if (power_difference < -maximum)
+    power_difference = -maximum;
+
+  if (power_difference < 0)
+    set_motors(maximum + power_difference, maximum);
+  else
+    set_motors(maximum, maximum - power_difference);
+  } 
+}
+
+void setup () {
+  delay(1000);
+  unsigned int counter; // used as a simple timer
+
+  // This must be called at the beginning of 3pi code, to set up the
+  // sensors.  We use a value of 2000 for the timeout, which
+  // corresponds to 2000*0.4 us = 0.8 ms on our 20 MHz processor.
+  robot.init(2000);
+  // Auto-calibration: turn right and left while calibrating the
+  // sensors.
+  for (counter=0; counter<80; counter++)
+  {
+    if (counter < 20 || counter >= 60)
+      OrangutanMotors::setSpeeds(40, -40);
+    else
+      OrangutanMotors::setSpeeds(-40, 40);
+
+    // This function records a set of sensor readings and keeps
+    // track of the minimum and maximum values encountered.  The
+    // IR_EMITTERS_ON argument means that the IR LEDs will be
+    // turned on during the reading, which is usually what you
+    // want.
+    robot.calibrateLineSensors(IR_EMITTERS_ON);
+
+    // Since our counter runs to 80, the total delay will be
+    // 80*20 = 1600 ms.
+    delay(20);
+  }
+  OrangutanMotors::setSpeeds(0, 0);
+}
+
+void loop () {
+  LINE_func();
+}
