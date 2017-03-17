4f7326fcc2d6871a7f055bdabbc23fc447a4db6f
TheManWithTheGit <TheManWithTheGit@users.noreply.github.com>
2017-03-17 14:55:48 +0000

    Slight cleaning up of code order

--- line_follower.ino
+++ line_follower.ino
@@ -8,57 +8,14 @@
 #include <OrangutanBuzzer.h>
 void motors_init(void);
 
-OrangutanLCD lcd;
+OrangutanLCD lcd; //reassigning the libraries to something easier to type
 OrangutanAnalog analog;
-
 Pololu3pi robot;
 unsigned int sensors[5]; // an array to hold sensor values
 unsigned int last_proportional = 0;
 long integral = 0;
 int counter = 0;
 
-void LINE_func(void) {
- //defining and initialising ldrs for line follow to zero.
-  while (1) {
-  // Get the position of the line.  Note that we *must* provide
-  // the "sensors" argument to read_line() here, even though we
-  // are not interested in the individual sensor readings.
-  unsigned int position = robot.readLine(sensors, IR_EMITTERS_ON);
-
-  // The "proportional" term should be 0 when we are on the line.
-  int proportional = (int)position - 2000;
-
-  // Compute the derivative (change) and integral (sum) of the
-  // position.
-  int derivative = proportional - last_proportional;
-  integral += proportional;
-
-  // Remember the last position.
-  last_proportional = proportional;
-
-  // Compute the difference between the two motor power settings,
-  // m1 - m2.  If this is a positive number the robot will turn
-  // to the right.  If it is a negative number, the robot will
-  // turn to the left, and the magnitude of the number determines
-  // the sharpness of the turn.  You can adjust the constants by which
-  // the proportional, integral, and derivative terms are multiplied to
-  // improve performance.
-  int power_difference = proportional/20 + integral/10000 + derivative*3/2;
-
-  // Compute the actual motor settings.  We never set either motor
-  // to a negative value.
-  const int maximum = 60; //limiter if the difference is too high, will only travel at this speed
-  if (power_difference > maximum)
-    power_difference = maximum;
-  if (power_difference < -maximum)
-    power_difference = -maximum;
-
-  if (power_difference < 0) //otherwise use the unchanged value to the motors.
-    set_motors(maximum + power_difference, maximum);
-  else
-    set_motors(maximum, maximum - power_difference);
-  } 
-}
 
 void setup () {
   delay(1000);
@@ -93,4 +50,46 @@ void setup () {
 
 void loop () {
   LINE_func();
+}
+
+void LINE_func(void) {
+ //defining and initialising ldrs for line follow to zero.
+  while (1) {
+  // Get the position of the line.  Note that we *must* provide
+  // the "sensors" argument to read_line() here, even though we
+  // are not interested in the individual sensor readings.
+  unsigned int position = robot.readLine(sensors, IR_EMITTERS_ON);
+
+  // The "proportional" term should be 0 when we are on the line.
+  int proportional = (int)position - 2000;
+
+  // Compute the change in position and the current absolute position
+  int derivative = proportional - last_proportional;
+  integral += proportional;
+
+  // Remember the last position.
+  last_proportional = proportional;
+
+  // Compute the difference between the two motor power settings,
+  // m1 - m2.  If this is a positive number the robot will turn
+  // to the right.  If it is a negative number, the robot will
+  // turn to the left, and the magnitude of the number determines
+  // the sharpness of the turn.  You can adjust the constants by which
+  // the proportional, integral, and derivative terms are multiplied to
+  // improve performance.
+  int power_difference = proportional/20 + integral/10000 + derivative*3/2;
+
+  // Compute the actual motor settings.  We never set either motor
+  // to a negative value.
+  const int maximum = 60; //limiter if the difference is too high, will only travel at this speed
+  if (power_difference > maximum)
+    power_difference = maximum;
+  if (power_difference < -maximum)
+    power_difference = -maximum;
+
+  if (power_difference < 0) //otherwise use the unchanged value to the motors.
+    set_motors(maximum + power_difference, maximum);
+  else
+    set_motors(maximum, maximum - power_difference);
+  } 
 }
